0ac4ee840824151aa4ae826f19b730b459870227
jose-117 <josekilo.07@gmail.com>
2025-07-17 14:26:53 -0600

    Cambios en el models de video_streaming

--- animeneon/settings.py
+++ animeneon/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'drf_yasg',
+    'taggit',
     'rest_framework',
     'video_streaming',
 ]
--- video_streaming/models.py
+++ video_streaming/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
 class Video(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     video_file = models.FileField(upload_to='videos/')
     created_at = models.DateTimeField(auto_now_add=True)
+    status_emision = models.BooleanField(default="False")
+    labels = TaggableManager()
 
     def __str__(self):
         return self.title
